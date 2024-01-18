1af42a02063d3ba4d79a5c5f2974077bea5c1969
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-01-19 00:23:36 +0200

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from sys import *
-if len(argv) > 1:
-    n = 1
-    for i in range(1, len(argv)):
-        print("{}: {}".format(n, argv[i]))
-        n += 1
-else:
-    print("0 arguments.")
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        n = 1
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(n, sys.argv[i]))
+            n += 1
+    else:
+        print("0 arguments.")
