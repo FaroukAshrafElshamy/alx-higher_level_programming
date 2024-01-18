fc907f8193d304930948ceb0567dced71b9e8e97
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-01-19 00:33:31 +0200

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    z = len(sys.argv)
-    if z > 1:
-        n = 1
-        if z == 1:
-            print("{} argument:".format(z-1))
-        else:
-            print("{} arguments:".format(z-1))
-
-        for i in range(1, z):
-            print("{}: {}".format(n, sys.argv[i]))
-            n += 1
+z = len(sys.argv)
+if z > 1:
+    n = 1
+    if z-1 == 1:
+        print("{} argument:".format(z-1))
     else:
-        print("0 arguments.")
+        print("{} arguments:".format(z-1))
+
+    for i in range(1, z):
+        print("{}: {}".format(n, sys.argv[i]))
+        n += 1
+else:
+    print("0 arguments.")
