93cf33778f1183f6ba31727627b3fa8e0af6dce3
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-01-19 00:26:04 +0200

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) > 1:
         n = 1
+        print("{} arguments.".format(len(sys.argv)-1))
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(n, sys.argv[i]))
             n += 1
