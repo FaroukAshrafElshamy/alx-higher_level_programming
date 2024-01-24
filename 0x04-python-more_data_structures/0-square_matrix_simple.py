e37904509edbe20b4e49d824870ad2eb93458551
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-01-24 13:50:05 +0200

    Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/bin/usr/python3
+def square_matrix_simple(matrix=[]):
+    m2 = []
+    for i in range(len(matrix)):
+        copy = list(map((lambda x: x**2), matrix[i]))
+        m2.append(copy)
+    return m2
