5abcfe8449ab296fb40088eb5abef4d19065e03d
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-04 19:39:15 +0200

    Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary.keys():
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = value
-print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
