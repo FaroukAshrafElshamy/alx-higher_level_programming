3580ea597acd2d096ee92392839322038e0b0f31
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-04 19:57:40 +0200

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        temp = list(a_dictionary.values())
+        temp2 = list(a_dictionary.keys())
+        item = temp.index(max(temp))
+        return temp2[item]
