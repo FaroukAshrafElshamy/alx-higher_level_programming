de7453282a2114478cc8ce8ce04c1b2037bc1262
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-04 23:29:50 +0200

    Roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,11 +6,8 @@ def roman_to_int(roman_string):
         rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         outnum = 0
         for i in roman_string:
-            if rom[i]:
-                if outnum > 0 and outnum < rom[i]:
-                    outnum = rom[i] - outnum
-                else:
-                    outnum += rom[i]
+            if outnum > 0 and outnum < rom[i]:
+                outnum = rom[i] - outnum
             else:
-                return 0
+                outnum += rom[i]
         return outnum
