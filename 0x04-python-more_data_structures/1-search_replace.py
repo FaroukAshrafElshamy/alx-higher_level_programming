514560b5b4f86104a5acc75a53a1e59f3f6a5669
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-04 18:31:49 +0200

    Search & Replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
+    temp = my_list.copy()
     for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return my_list
+        if temp[i] == search:
+            temp[i] = replace
+    return temp
