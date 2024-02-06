0f9e5b5f8d4877bd97cac9241d0809e26a3e0fd4
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-06 14:59:51 +0200

    Safe Print List

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,5 +8,6 @@ def safe_print_list(my_list=[], x=0):
             i += 1
         except IndexError:
             break
-    print(int(List))
+    if List != '':
+        print(int(List))
     return i
