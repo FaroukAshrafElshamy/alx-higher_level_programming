c44bce0501b33d5cc7769ebc53d84e3d73d2a075
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-06 23:00:33 +0200

    Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Initilizing a new private inistance from Squalre class"""
+
+
+class Square:
+    """Initilization method"""
+    def __init__(self, size):
+        self.__size = size
