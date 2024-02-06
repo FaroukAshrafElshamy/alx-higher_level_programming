7195356fc2b2968b2fff35ed9c42acf7c496bc8a
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-07 01:22:12 +0200

    Single Linked List

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__next_node = value
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None or not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__data = value
+
+
+class SinglyLinkedList:
+    def __init__(self, head):
+        self.__head = head
+
+    def sorted_insert(self, value):
+        new = Node(value)
+        if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while tmp.next_node is not None and tmp.next_node.data < value:
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return "\n".join(values)
