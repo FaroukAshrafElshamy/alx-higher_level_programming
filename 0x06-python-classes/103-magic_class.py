481cfe739e59804629425e3b697a0632129673d6
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-07 01:52:43 +0200

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import math
 """Initilizing a new private inistance from Squalre class"""
+import math
 
 
 class MagicClass:
-    """Initilization method"""
+    """Represent a circle."""
+
     def __init__(self, radius=0):
+
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
