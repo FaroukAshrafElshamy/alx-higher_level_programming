b191ce5174a673800e588ae8d6a8403a16c359a9
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-06 23:28:28 +0200

    Area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Initilizing a new private inistance from Squalre class"""
+
+
+class Square:
+    """Initilization method"""
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.size**2
