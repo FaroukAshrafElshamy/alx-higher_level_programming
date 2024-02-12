aad660616d2510b15d8f5ef7d3d3345aa3113a95
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-13 01:47:21 +0200

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
 
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     """ Initilization methon (Constructor) """
     def __init__(self, width=0, height=0):
