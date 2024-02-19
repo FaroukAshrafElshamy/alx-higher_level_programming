aa2c4d5a727e480789ad2f25d80da8679b946885
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-19 23:02:25 +0200

    My list

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    if not isinstance(a, int) or not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    elif not isinstance(b, int) or not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    else:
+        return int(a) + int(b)
