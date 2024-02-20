c3990dd7b62829aa0a989f9b2b35295ee1149e00
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-21 01:49:35 +0200

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" Read from a file using with statement """
+""" A function that takes a file name """
 
 
 def read_file(filename=""):
+    """ Read from a file using with statement """
     with open(filename, encoding="utf-8") as File:
         print(File.read(), end='')
