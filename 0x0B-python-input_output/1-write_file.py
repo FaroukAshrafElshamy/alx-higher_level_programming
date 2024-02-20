5f7fb2dc5fafbd3a6872fc6d9b595e445b4e42ae
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-21 01:55:24 +0200

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A function that takes a file """
+
+
+def write_file(filename="", text=""):
+    """ Write text into the file and overwrite its content """
+    with open(filename, encoding="utf-8") as File:
+        return File.write(text)
