bb4e75739982a9089b6cb3a74040c643210729b2
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-02-21 01:57:06 +0200

    Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """ Write text into the file and overwrite its content """
-    with open(filename, encoding="utf-8") as File:
+    with open(filename, "w", encoding="utf-8") as File:
         return File.write(text)
