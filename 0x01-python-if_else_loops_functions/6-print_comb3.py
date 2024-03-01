cc466041f161b6a4f96934e023c9e9b4c7ac6f10
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-03-01 23:12:53 +0200

    Unique numbers

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for i in range(90):
         if i != 89:
             print("{:02d}".format(i), end=', ')
         else:
-            print("{:02d}".format(i), end=', ')
+            print("{:02d}".format(i))
