c7631944f20464867fbe323ee5b8ad16645316e0
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-03-01 23:33:54 +0200

    Last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    if number > 10:
-        print("{}".format(number % 10), end='')
-    else:
-        print("{}".format(number), end='')
+    print("{}".format(abs(number) % 10), end='')
