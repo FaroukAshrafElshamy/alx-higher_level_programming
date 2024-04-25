50b868f0dbc57a0f6d8ef8b730c2ef2576bce968
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-04-25 17:59:55 +0200

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const array = [];
-for (let i = 2; i < process.argv.length; i++) {
+for (let i = 2; i < (process.argv.length); i++) {
   array.push(process.argv[i]);
 }
-array.sort();
+array.sort((a, b) => b - a);
 if (isNaN(array[1]) || array[1] === '1') {
   console.log(0);
 } else {
-  console.log(array[array.length - 2]);
+  console.log(array[1]);
 }
