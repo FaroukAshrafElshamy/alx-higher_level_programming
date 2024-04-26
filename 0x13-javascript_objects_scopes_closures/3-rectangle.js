8fec39a73abfd9e93c10920a66b0adc1a50f1b70
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-04-26 21:19:15 +0300

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,8 +8,8 @@ module.exports = class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.h; i++) {
-      console.log('X'.repeat(this.w));
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 };
