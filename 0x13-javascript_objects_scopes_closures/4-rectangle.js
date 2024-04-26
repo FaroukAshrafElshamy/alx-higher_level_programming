43126a2da42de9e24059be05265fd1e36d6b9eda
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-04-26 21:30:33 +0300

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -24,17 +24,3 @@ module.exports = class Rectangle {
     this.width = this.width * 2;
   }
 };
-
-const Rectangle = require('./4-rectangle');
-
-const r1 = new Rectangle(2, 3);
-console.log('Normal:');
-r1.print();
-
-console.log('Double:');
-r1.double();
-r1.print();
-
-console.log('Rotate:');
-r1.rotate();
-r1.print();
