bebf06706cd59c17cdfa64fb417bc88b3ad6b3e3
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-05-10 09:53:34 +0300

    What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This script fetches data from
+    https://alx-intranet.hbtn.io/status
+"""
+
+
+from urllib import request
+url = "https://alx-intranet.hbtn.io/status"
+with request.urlopen(url) as res:
+    data = res.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(data)))
+    print("\t- content: {}".format(data))
+    print("\t- utf8 content: {}".format(data.decode("utf8")))
