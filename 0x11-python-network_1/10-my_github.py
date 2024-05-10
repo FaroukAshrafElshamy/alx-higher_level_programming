2af8826892c7a71b34f9bf145f406a1644491371
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-05-11 00:22:05 +0300

    My GitHub

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+
 """
-Python script that takes your Github credentials (username and password)
-and uses the Github API to display your id
+Write a script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id.
 """
 
-
+import sys
 import requests
 from requests.auth import HTTPBasicAuth
-from sys import argv
 
-if __name__ == '__main__':
-    url = 'https://api.github.com/users/{}'.format(argv[1])
-    R = requests.get(url,
-                     auth=HTTPBasicAuth(argv[1], argv[2]))
-    print(R.json().get('id'))
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    request = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    print(request.json().get('id'))
