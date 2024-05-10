827ec426770979229ed7eb11dd677e9dc112c4f3
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-05-10 17:57:09 +0300

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,9 +9,10 @@ and displays the body of the response (decoded in utf-8)
 import sys
 from urllib import request, parse
 
-url = sys.argv[1]
-val = {"email": sys.argv[2]}
-data = parse.urlencode(val).encode("ascii")
-req = request.Request(url, data)
-with request.urlopen(req) as R:
-    print(R.read().decode('utf-8'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    val = {"email": sys.argv[2]}
+    data = parse.urlencode(val).encode("ascii")
+    req = request.Request(url, data)
+    with request.urlopen(req) as R:
+        print(R.read().decode('utf-8'))
